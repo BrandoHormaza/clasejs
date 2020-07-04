cdc363d81aeb9d9bd014f236e3477e9a0e801ae0
BrandoHormaza <brandonbohorquez0105@gmail.com>
2020-07-04 13:03:04 -0500

    trabajo pendiente
    trbajo en clase

---
+++ clase.js
@@ -0,0 +1,22 @@
+console.log("hola mundo");
+var cuadro = [
+    ['g','h','a','q','o'], 
+    ['p','a','n','r','h'], 
+    ['c','l','r','a','m'], 
+    ['p','e','f','r','g'], 
+    ['p','w','x','z','a'],
+]; 
+
+console.log(cuadro)
+console.log(cuadro[0][0],cuadro[1][1],cuadro[2][2],cuadro[3][3],cuadro[4][4])
+console.log(cuadro[4][0],cuadro[3][1],cuadro[2][2],cuadro[1][3],cuadro[0][4])
+
+
+var edad = 24;
+
+if (edad < 18) 
+  console.log('tu eres mayor de edad');
+}
+
+
+}
